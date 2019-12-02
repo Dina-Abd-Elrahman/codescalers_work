bdc49bd73848ef135cd973ba1ad6fcd8b6da8ebb
dinaabdo <abdelrad@codescalers.com>
2019-12-02 16:17:13 +0200

    Update mail client

--- jsx_clients/test/test_mail_client.py
+++ jsx_clients/test/test_mail_client.py
@@ -85,5 +85,5 @@ class MailClient(BaseTest):
             files=["/tmp/test_{}".format(RAND_NUM)],
         )
 
-        self.info("Make sure that message has been sent correctly, and check the existing of the file")
+        self.info("Make sure that message has been sent correctly, and check the existance of the file")
         self.assertTrue(self.check_inbox('attachment; filename="test_{}"'.format(RAND_NUM)))
