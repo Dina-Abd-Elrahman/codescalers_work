ad4aede52a692d92d1b6f0fe200e38e6e6adb3c1
dinaabdo <abdelrad@codescalers.com>
2020-08-13 16:35:59 +0200

    create test01_web.py

---
+++ jsx_builder/flist/test01_web.py
@@ -0,0 +1,27 @@
+import unittest
+from Jumpscale import j
+from .base_test import BaseTest
+from parameterized import parameterized
+
+
+class Web_TestCases(BaseTest):
+    @parameterized.expand(
+        [
+            ("caddy", "caddy"),
+            ("traefik", "traefik"),
+            ("nginx", "nginx"),
+            ("openresty", "resty"),
+            ("apachectl", "apache2"),
+            ("lapis", "lapis"),
+        ]
+    )
+    def test_web_flists(self, flist, binary):
+        """ SAN-001
+        *Test web builers sandbox*
+        """
+        self.info("run {} sandbox.".format(flist))
+        getattr(j.builders.web, flist).sandbox(**self.sandbox_args)
+        self.info("Deploy container with uploaded {} flist.".format(flist))
+        self.deploy_flist_container("{}".format(flist))
+        self.info("Check that {} flist works.".format(flist))
+        self.assertIn("Usage: ", self.check_container_flist(j.core.tools.text_replace("{DIR_BASE}/bin/{} -h").format(binary)))
